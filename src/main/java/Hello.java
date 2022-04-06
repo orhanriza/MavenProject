498b90490732f3a4664203f8efbeb4a4235f9ae5
orhan <orhanrizayildirim@gmail.com>
2022-04-06 16:17:07 -0400

    added more to hello

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("developing hello feature");
+        System.out.println("local reposatory is already published to Github");
+        System.out.println("doing more work");
     }
 }
